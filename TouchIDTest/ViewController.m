cbca6b71bf078aad599128eb414af5603aecc224
huzhizhong <371251480@qq.com>
2015-09-22 09:50:04 +0800

    TouchID
    TouchID Test First Commit

--- TouchIDTest/ViewController.m
+++ TouchIDTest/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <LocalAuthentication/LocalAuthentication.h>
 
 @interface ViewController ()
 
@@ -17,6 +18,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self configAuthButton];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +26,41 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void) configAuthButton
+{
+    UIButton* button = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button setTitle:@"test touch ID" forState:UIControlStateNormal];
+    [button setFrame:CGRectMake((self.view.frame.size.width - 150)/2,
+                                (self.view.frame.size.height-30)/2, 150, 30)];
+    [button setTitleColor:[UIColor blackColor] forState:
+                                UIControlStateNormal];
+    [button setBackgroundColor:[UIColor orangeColor]];
+    [button addTarget:self action:@selector(authButton:)
+                                forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+}
+
+- (void) authButton:(UIButton* )sender
+{
+    LAContext* context = [[LAContext alloc]init];
+    NSError* error = nil;
+    NSString* reason = @"测试：验证touchID";
+    
+    if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&error]) {
+        [context evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics localizedReason:reason
+                                                                reply:^(BOOL success, NSError* error) {
+            NSString* text = nil;
+            if (success) {
+                text = @"验证成功";
+            } else {
+                text = @"请设置TouchID";
+            }
+            UIAlertView* alert = [[UIAlertView alloc] initWithTitle:@"提示" message:text delegate:self cancelButtonTitle:@"确定" otherButtonTitles:nil, nil];
+            [alert show];
+        }];
+    } else {
+        UIAlertView* alert = [[UIAlertView alloc] initWithTitle:@"提示" message:@"请设置TouchID" delegate:self cancelButtonTitle:@"确定" otherButtonTitles:nil, nil];
+        [alert show];
+    }
+}
 @end
